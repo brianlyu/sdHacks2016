45deb0836b10b56cf7c5f532c5caf59e45fe607d
Edward Huang <egunawan@uscd.edu>
2016-10-01 03:50:05 -0700

    front page CSS and title

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
+/* tell express to look at the static page in public dir */
+app.use(express.static('public'));
+
+/* view engine for ejs template engine */
+app.set("view engine", "ejs");
+
+
 app.get('/', (reg, res) => {
-  res.send("Music App!");
+  res.render("pages/home"); // started from the views dir
 });
+
 
 
 app.listen(3000, ()=>{
